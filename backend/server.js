594b9edd1f981633cbeb734f18c30a30c1fec93c
Andrew Park <andysampark@gmail.com>
2022-07-06 16:37:48 -0400

    build script

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const dotenv = require("dotenv").config()
 const port = process.env.PORT || 5000
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 const connectDB = async () => {
     try {
@@ -27,8 +27,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/", require("./routes/noteRoute"))
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.listen(port, function() {
+app.get('*', (req, res) =>
+    res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+);
+
+
+app.listen(port, function () {
     console.log(`listening on port ${port}`)
 })
 
